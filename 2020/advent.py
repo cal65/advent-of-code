6578daef108d3178fb6c38fa28a6195f018acb86
Cal Lee <cal.christopheralee@gmail.com>
2021-01-02 00:34:33 -0500

    feat: solve for day 11 part 1
    And added black formatting

--- 2020/advent.py
+++ 2020/advent.py
@@ -5,182 +5,364 @@ from functools import reduce
 import operator
 import re
 
-def day1(file, n):
-	 d1 = pd.read_table(file, header=None)[0]
-	 pairs = combinations(d1, n)
 
-	 def inner_arith(x: tuple, target_sum):
-	 	if sum(x) == target_sum:
-	 		return reduce((lambda x, y: x * y), x)
-	 	else:
-	 		return 0
-	 val = 0	
-	 for pair in pairs:
-	 	val += inner_arith(pair, 2020)
-	 return val
+def day1(file, n):
+    d1 = pd.read_table(file, header=None)[0]
+    pairs = combinations(d1, n)
+
+    def inner_arith(x: tuple, target_sum):
+        if sum(x) == target_sum:
+            return reduce((lambda x, y: x * y), x)
+        else:
+            return 0
+
+    val = 0
+    for pair in pairs:
+        val += inner_arith(pair, 2020)
+    return val
 
 
 def read_df(file):
-	d2 = pd.read_table(file, header=None)[0]
-	split_tuples = list(d2.str.split(': '))
-	password_df = pd.DataFrame.from_records(split_tuples, columns = ['policy', 'password'])
-	return password_df
+    d2 = pd.read_table(file, header=None)[0]
+    split_tuples = list(d2.str.split(": "))
+    password_df = pd.DataFrame.from_records(
+        split_tuples, columns=["policy", "password"]
+    )
+    return password_df
+
 
 def transform_df(df, col):
-	split_df = df[col].str.split(' |-').apply(pd.Series) 
-	split_df.columns = ['min', 'max', 'letter']
-	split_df['min'] = split_df['min'].astype(int)
-	split_df['max'] = split_df['max'].astype(int)
-	df = pd.concat([df, split_df], axis=1)
-	return df
+    split_df = df[col].str.split(" |-").apply(pd.Series)
+    split_df.columns = ["min", "max", "letter"]
+    split_df["min"] = split_df["min"].astype(int)
+    split_df["max"] = split_df["max"].astype(int)
+    df = pd.concat([df, split_df], axis=1)
+    return df
+
 
 def day2(file):
-	password_df = read_df(file)
-	password_df = transform_df(password_df, 'policy')
-	password_df['count'] = password_df.apply(lambda x: x.password.count(x.letter), axis=1)
-	password_df['pass'] = (password_df['count'] >= password_df['min']) & (password_df['count'] <= password_df['max'])
+    password_df = read_df(file)
+    password_df = transform_df(password_df, "policy")
+    password_df["count"] = password_df.apply(
+        lambda x: x.password.count(x.letter), axis=1
+    )
+    password_df["pass"] = (password_df["count"] >= password_df["min"]) & (
+        password_df["count"] <= password_df["max"]
+    )
 
-	return password_df['pass'].sum()
+    return password_df["pass"].sum()
+
 
 def day2b(file):
-	password_df = read_df(file)
-	password_df = transform_df(password_df, 'policy')
-	password_df['position_1'] = password_df.apply(lambda x: x.password[x['min']-1] == x.letter, axis = 1) 
-	password_df['position_2'] = password_df.apply(lambda x: x.password[x['max']-1] == x.letter, axis = 1) 
-	password_df['pass'] = (password_df['position_1']) != (password_df['position_2']) 
-	return password_df['pass'].sum()
+    password_df = read_df(file)
+    password_df = transform_df(password_df, "policy")
+    password_df["position_1"] = password_df.apply(
+        lambda x: x.password[x["min"] - 1] == x.letter, axis=1
+    )
+    password_df["position_2"] = password_df.apply(
+        lambda x: x.password[x["max"] - 1] == x.letter, axis=1
+    )
+    password_df["pass"] = (password_df["position_1"]) != (password_df["position_2"])
+    return password_df["pass"].sum()
+
 
 def day3(file):
-	tree_grid = pd.read_table(file, header=None)[0]
-	rows = []
-	for row in tree_grid:
-		rows.append(list(row*300))
-	grid = np.matrix(rows)
-	return grid
+    tree_grid = pd.read_table(file, header=None)[0]
+    rows = []
+    for row in tree_grid:
+        rows.append(list(row * 300))
+    grid = np.matrix(rows)
+    return grid
 
-#recursive solution
-def traverse(grid, start_row, start_col, x, y, tree_char = "#"):
-	grid_row, grid_col = grid.shape
-	if (start_row + y > grid_row) | (start_col + x > grid_col):
-		return 0
-	else:
-		value = 1 if grid[start_row, start_col] == tree_char else 0
-		return value + traverse(grid, start_row + y, start_col + x, x, y, tree_char = tree_char)
+
+# recursive solution
+def traverse(grid, start_row, start_col, x, y, tree_char="#"):
+    grid_row, grid_col = grid.shape
+    if (start_row + y > grid_row) | (start_col + x > grid_col):
+        return 0
+    else:
+        value = 1 if grid[start_row, start_col] == tree_char else 0
+        return value + traverse(
+            grid, start_row + y, start_col + x, x, y, tree_char=tree_char
+        )
 
 
 def traverse_part_two(grid):
-	slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
-	options = []
-	for slope in slopes:
-		options.append(traverse(grid, 0, 0, slope[0], slope[1]))
-	return reduce(operator.mul, options)
+    slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    options = []
+    for slope in slopes:
+        options.append(traverse(grid, 0, 0, slope[0], slope[1]))
+    return reduce(operator.mul, options)
+
 
 def day4(file):
-	 d4 = pd.read_table(file, header=None, skip_blank_lines=False)[0]
-	 return d4
+    d4 = pd.read_table(file, header=None, skip_blank_lines=False)[0]
+    return d4
+
 
 def parse_passport(l):
-	passport_list = []
-	passport = {}
-	for line in l:
-		if pd.isnull(line):
-			passport_list.append(passport)
-			passport = {}
-		else:
-			line_split = line.split(' ')
-			for split in line_split:
-				pair = split.split(':')
-				passport[pair[0]] = pair[1]
-	# this isn't ideal, but because we aren't parsing the last NA, the final loop is not appended
-	passport_list.append(passport)
-	return passport_list
+    passport_list = []
+    passport = {}
+    for line in l:
+        if pd.isnull(line):
+            passport_list.append(passport)
+            passport = {}
+        else:
+            line_split = line.split(" ")
+            for split in line_split:
+                pair = split.split(":")
+                passport[pair[0]] = pair[1]
+    # this isn't ideal, but because we aren't parsing the last NA, the final loop is not appended
+    passport_list.append(passport)
+    return passport_list
+
 
 def check_passport(passport, required_fields):
-	return all(x in passport.keys() for x in required_fields)
+    return all(x in passport.keys() for x in required_fields)
 
-required_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+
+required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+
 
 def count_valid_passports(file):
-	passport_list = parse_passport(day4(file))
-	return  sum([check_passport(passport, required_fields) for passport in passport_list])
-	
+    passport_list = parse_passport(day4(file))
+    return sum(
+        [check_passport(passport, required_fields) for passport in passport_list]
+    )
+
+
 def test_day4():
-	assert(check_passport({'ecl':'gry', 'pid':'860033327', 'eyr':'2020', 'hcl':'#fffffd',
-		'byr':'1937', 'iyr':'2017', 'cid':'147', 'hgt': '183cm'}, required_fields) == 1)
-	assert(check_passport({'iyr':'2013', 'ecl':'amb', 'cid':'350', 'eyr':'2023', 'pid':'028048884',
-		'hcl':'#cfa07d', 'byr':'1929'}, required_fields) == 0)
-	assert(check_passport({'hcl':'#ae17e1', 'iyr':'2013',
-		'eyr':'2024',
-		'ecl':'brn', 'pid':'760753108', 'byr':'1931',
-		'hgt':'179cm'}, required_fields) == 1)
-	assert( check_passport({'hcl':'#cfa07d', 'eyr':'2025', 'pid':'166559648',
-		'iyr':'2011', 'ecl':'brn', 'hgt':'59in'}, required_fields)== 0)
+    assert (
+        check_passport(
+            {
+                "ecl": "gry",
+                "pid": "860033327",
+                "eyr": "2020",
+                "hcl": "#fffffd",
+                "byr": "1937",
+                "iyr": "2017",
+                "cid": "147",
+                "hgt": "183cm",
+            },
+            required_fields,
+        )
+        == 1
+    )
+    assert (
+        check_passport(
+            {
+                "iyr": "2013",
+                "ecl": "amb",
+                "cid": "350",
+                "eyr": "2023",
+                "pid": "028048884",
+                "hcl": "#cfa07d",
+                "byr": "1929",
+            },
+            required_fields,
+        )
+        == 0
+    )
+    assert (
+        check_passport(
+            {
+                "hcl": "#ae17e1",
+                "iyr": "2013",
+                "eyr": "2024",
+                "ecl": "brn",
+                "pid": "760753108",
+                "byr": "1931",
+                "hgt": "179cm",
+            },
+            required_fields,
+        )
+        == 1
+    )
+    assert (
+        check_passport(
+            {
+                "hcl": "#cfa07d",
+                "eyr": "2025",
+                "pid": "166559648",
+                "iyr": "2011",
+                "ecl": "brn",
+                "hgt": "59in",
+            },
+            required_fields,
+        )
+        == 0
+    )
+
 
 def check_valid_passports_b(file):
-	passport_list = parse_passport(day4(file))
-	valid_passports = [p for p in passport_list if check_passport(p, required_fields)]
-	check_b = [check_passport_fields(x) for x in valid_passports]   
-	return sum(check_b)
+    passport_list = parse_passport(day4(file))
+    valid_passports = [p for p in passport_list if check_passport(p, required_fields)]
+    check_b = [check_passport_fields(x) for x in valid_passports]
+    return sum(check_b)
+
 
 def check_passport_fields(passport):
-	return all([_check_byr(passport),
-		_check_eyr(passport),
-		_check_iyr(passport),
-	   	_check_hgt(passport),		
-	   	_check_hcl(passport),
-	   	_check_ecl(passport),
-	   	_check_pid(passport)]) 
+    return all(
+        [
+            _check_byr(passport),
+            _check_eyr(passport),
+            _check_iyr(passport),
+            _check_hgt(passport),
+            _check_hcl(passport),
+            _check_ecl(passport),
+            _check_pid(passport),
+        ]
+    )
+
 
 def _check_yr(passport, yr_field, low, high):
-	try:
-		yr = int(passport[yr_field])
-	except:
-		return False
-	return (yr >= low) & (yr <= high)
+    try:
+        yr = int(passport[yr_field])
+    except:
+        return False
+    return (yr >= low) & (yr <= high)
+
 
 def _check_byr(passport):
-	return _check_yr(passport, 'byr', 1920, 2002)
+    return _check_yr(passport, "byr", 1920, 2002)
+
 
 def _check_iyr(passport):
-	return _check_yr(passport, 'iyr', 2010, 2020)
+    return _check_yr(passport, "iyr", 2010, 2020)
+
 
 def _check_eyr(passport):
-	return _check_yr(passport, 'eyr', 2020, 2030)
+    return _check_yr(passport, "eyr", 2020, 2030)
+
 
 def _check_hgt(passport):
-	hgt_raw = passport['hgt']
-	try:
-		hgt = int(re.findall('[0-9]+', hgt_raw)[0])
-	except:
-		return False
-	try:
-		unit = re.findall('[a-z]+', hgt_raw)[0]
-	except:
-		return False
-	if unit == 'cm':
-		return (hgt >= 150) & (hgt <= 193)
-	if unit == 'in':
-		return (hgt >= 59) & (hgt <= 76)
-	else:
-		return False
+    hgt_raw = passport["hgt"]
+    try:
+        hgt = int(re.findall("[0-9]+", hgt_raw)[0])
+    except:
+        return False
+    try:
+        unit = re.findall("[a-z]+", hgt_raw)[0]
+    except:
+        return False
+    if unit == "cm":
+        return (hgt >= 150) & (hgt <= 193)
+    if unit == "in":
+        return (hgt >= 59) & (hgt <= 76)
+    else:
+        return False
+
 
 def _check_hcl(passport):
-	hcl = passport['hcl']
-	return re.match('^\\#[a-z|0-9]{6}(?![0-9])', hcl) is not None
+    hcl = passport["hcl"]
+    return re.match("^\\#[a-z|0-9]{6}(?![0-9])", hcl) is not None
+
 
 def _check_ecl(passport):
-	ecl = passport['ecl']
-	return ecl in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+    ecl = passport["ecl"]
+    return ecl in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+
 
 def _check_pid(passport):
-	pid = passport['pid']
-	return re.match('[0-9]{9}(?![0-9])', pid) is not None
+    pid = passport["pid"]
+    return re.match("[0-9]{9}(?![0-9])", pid) is not None
 
 
 def test_check_valid_passport():
-	test_passport = {
-	'pid':'087499704', 'hgt':'74in', 'ecl':'grn',
-	'iyr':'2012', 'eyr':'2030', 'byr':'1980',
-	'hcl':'#623a2f'
-	}
-	assert(check_passport_fields(test_passport) == True)
+    test_passport = {
+        "pid": "087499704",
+        "hgt": "74in",
+        "ecl": "grn",
+        "iyr": "2012",
+        "eyr": "2030",
+        "byr": "1980",
+        "hcl": "#623a2f",
+    }
+    assert check_passport_fields(test_passport) == True
+
+
+def day_11(file):
+    d11 = pd.read_table(file, header=None, skip_blank_lines=False)[0]
+    d11 = convert_layout(d11)
+    return d11
+
+
+test_layout = [
+    "L.LL.LL.LL",
+    "LLLLLLL.LL",
+    "L.L.L..L..",
+    "LLLL.LL.LL",
+    "L.LL.LL.LL",
+    "L.LLLLL.LL",
+    "..L.L.....",
+    "LLLLLLLLLL",
+    "L.LLLLLL.L",
+    "L.LLLLL.LL",
+]
+
+
+def convert_layout(layout):
+    layout_lists = [list(lay) for lay in layout]
+    layout_array = np.array(layout_lists)
+    return layout_array
+
+
+def assess_seat(layout, row, column):
+    if layout[row, column] == ".":
+        return "."
+    elif layout[row, column] == "L":
+        if count_adjacent(layout, row, column) == 0:
+            return "#"
+        else:
+            return "L"
+    elif layout[row, column] == "#":
+        if count_adjacent(layout, row, column) >= 4:
+            return "L"
+        else:
+            return "#"
+
+
+def count_adjacent(layout, row, column):
+    def get_seat(layout, row, column):
+        if (
+            (row < 0)
+            or (column < 0)
+            or (row >= layout.shape[0])
+            or (column >= layout.shape[1])
+        ):
+            return "."
+        else:
+            return layout[row, column]
+
+    adjacent = (
+        int(get_seat(layout, row - 1, column - 1) == "#")
+        + int(get_seat(layout, row - 1, column) == "#")
+        + int(get_seat(layout, row - 1, column + 1) == "#")
+        + int(get_seat(layout, row, column - 1) == "#")
+        + int(get_seat(layout, row, column + 1) == "#")
+        + int(get_seat(layout, row + 1, column - 1) == "#")
+        + int(get_seat(layout, row + 1, column))
+        + int(get_seat(layout, row + 1, column + 1) == "#")
+    )
+    return adjacent
+
+
+def assess_layout(layout):
+    rows, columns = layout.shape
+    layout_copy = layout.copy()
+    for row in np.arange(rows):
+        for column in np.arange(columns):
+            layout_copy[row, column] = assess_seat(layout, row, column)
+    return layout_copy
+
+
+def simulate(layout):
+    passengers = [(layout == "#").sum()]
+    layout = assess_layout(layout)
+    passengers.append((layout == "#").sum())
+    i = 0
+    while passengers[-2] != passengers[-1]:
+        layout = assess_layout(layout)
+        passengers.append((layout == "#").sum())
+        i += 1
+    return passengers[-1], i
