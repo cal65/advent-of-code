873fc86c7e96f505a6e25d9575e0eb2ff62cc365
Cal Lee <cal.christopheralee@gmail.com>
2022-03-24 22:52:37 -0700

    day 2

--- 2021/day2.py
+++ 2021/day2.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 import argparse
+from functools import lru_cache
+import sys
+sys.setrecursionlimit(10000)
+
 
 def read_file(file_path):
 	num = pd.read_table(file_path, header=None)
@@ -29,6 +33,26 @@ def analyze_position(text, x, y):
 		x, y = analyze_position(text[1:], x, y)
 		return x, y
 
+def analyze_aim(direction, distance, x, y, aim):
+	if direction == 'up':
+		aim -= distance
+	elif direction == 'down':
+		aim += distance
+	elif direction == 'forward':
+		x += distance
+		y += distance * aim
+	return x, y, aim
+
+#@lru_cache()
+def analyze_position_aim(text, x, y, aim):
+	parts = split_text(text[0], ' ')
+	x, y, aim = analyze_aim(parts[0], int(parts[1]), x, y, aim)
+	if len(text) == 1:
+		return x, y, aim
+	else:
+		x, y, aim = analyze_position_aim(text[1:], x, y, aim)
+		return x, y, aim
+
 if __name__ == "__main__":
     """
     Usage: python append_to_export.py filepath.csv --username [--update] [wait]
@@ -38,5 +62,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     file_path = args.file_path
     text = read_file(file_path)
-    x, y = analyze_position(text, 0, 0)
+    x, y, aim = analyze_position_aim(text, 0, 0, 0)
     print(x * y)
