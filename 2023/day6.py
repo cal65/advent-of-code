0dff72cd16bb206e6743cf3ec0fa1ca758fc65fe
Cal Lee <cal.christopheralee@gmail.com>
2024-09-13 09:26:50 -0700

    feat: finished day 6
    Simple quadratic equation solution

--- 2023/day6.py
+++ 2023/day6.py
@@ -19,6 +19,13 @@ class Boat_race():
 		return len(winning_distances)
 
 
+def quadratic(t, d):
+	# equation is x**2 -xt + d <= 0
+	discriminant = np.sqrt(t**2 - 4*d)
+	x1 = (t + discriminant)/2
+	x2 = (t - discriminant)/2
+	return x1, x2
+
 
 if __name__ == '__main__':
 	d6 = read_file('day6.txt')
@@ -28,14 +35,25 @@ if __name__ == '__main__':
 	times = [int(t) for t in times_raw[1:] if t != '']
 	boats = []
 	winnings_list = []
+
+	test_boats = [Boat_race(7, 9), Boat_race(15, 40), Boat_race(30, 200)]
+	test_wins = [b.iterate_races() for b in test_boats]
+	print(f"Test: ")
+	print(test_wins)
 	for t, d in zip(times, distances):
 		boat = Boat_race(t, d)
 		boats.append(boat)
 		winnings_list.append(boat.iterate_races())
-	print(winnings_list)
+	print("Solution: ")
 	print(np.prod(winnings_list))
-    
-	test_boats = [Boat_race(7, 9), Boat_race(15, 40), Boat_race(30, 200)]
-	test_wins = [b.iterate_races() for b in test_boats]
-	print('test wins')
-	print(test_wins)
+	single_t = d6[0].replace(' ', '').replace('Time:', '')
+	single_t = int(single_t)
+	single_d = d6[1].replace(' ', '').replace('Distance:', '')
+	single_d = int(single_d)
+	x1, x2 = quadratic(single_t, single_d)
+	x1_round = round(x1) - 1
+	x2_round = round(x2) + 1
+	b = Boat_race(time=single_t, record_distance=single_d)
+	print(b.race(x2_round) - single_d)
+	print(f"Answer is {str(x1_round - x2_round+1)}")
+
