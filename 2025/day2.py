61207b185cc77ad8e7b887d8917890eddc812d97
Cal Lee <cal.christopheralee@gmail.com>
2025-12-04 20:30:35 -0800

    feat: frames

---
+++ 2025/day2.py
@@ -0,0 +1,22 @@
+from utils import *
+import numpy as np
+
+test_data = []
+expected_answer = []
+
+def solve(input):
+    return
+
+def test_part_a(input, expected_answer):
+    return True
+
+def test_part_b():
+    return True
+
+
+if __name__ == "__main__":
+    file_raw = read_input("day2.txt")
+    input_data = file_raw.split("\n")
+
+    print(test_part_b(test_data, expected_answer))
+    answer = solve(input_data)
